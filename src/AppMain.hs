9bfa1fe3f6ab14f340b6a4ab37c980f6deefc4ce
hiogawa <hi.ogawa.zz@gmail.com>
2016-07-12 20:23:55 +0900

    read environment variable at runtime

--- src/AppMain.hs
+++ src/AppMain.hs
@@ -6,14 +6,11 @@ import Yesod (toWaiApp)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import System.Environment (lookupEnv)
-import Data.Maybe (fromJust)
 
 import App (App(..))
-import EnvVarLookup (envVarLookup)
-
 
 main :: IO ()
 main = do
-  waiApp <- toWaiApp App { connection = TE.encodeUtf8 . T.pack $ $(envVarLookup "APP_DATABASE") }
-  -- NOTE: need to recognize port number runtime for Heroku deployment :(
-  flip run waiApp =<< read . fromJust <$> lookupEnv "PORT"
+  Just port <- lookupEnv "PORT"
+  Just conn <- lookupEnv "APP_DATABASE"
+  run (read port) =<< toWaiApp App { connection = TE.encodeUtf8 . T.pack $ conn }
